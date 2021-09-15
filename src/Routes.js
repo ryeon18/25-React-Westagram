81aca87ecd720e801302fb702a269b1d30adbfd2
Jeong-minji <alswl4152@naver.com>
2021-09-15 17:16:09 +0900

    Modify: change components name

--- src/Routes.js
+++ src/Routes.js
@@ -2,34 +2,34 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // 민지's 컴포넌트
-import LoginminjiJeong from "./pages/minjiJeong/Login/Login";
-import MainminjiJeong from "./pages/minjiJeong/Main/Main";
+import LoginMinji from "./pages/minjiJeong/Login/Login";
+import MainMinji from "./pages/minjiJeong/Main/Main";
 
 // 수민's 컴포넌트
-import LoginsuminKim from "./pages/suminKim/Login/Login";
-import MainsuminKim from "./pages/suminKim/Main/Main";
+import LoginSumin from "./pages/suminKim/Login/Login";
+import MainSumin from "./pages/suminKim/Main/Main";
 
 // 세연's 컴포넌트
-import LoginseyeonPark from "./pages/seyeonPark/Login/Login";
-import MainseyeonPark from "./pages/seyeonPark/Main/Main";
+import LoginSeyeon from "./pages/seyeonPark/Login/Login";
+import MainSeyeon from "./pages/seyeonPark/Main/Main";
 
 // 선호's 컴포넌트
-import LoginsunhoLee from "./pages/sunhoLee/Login/Login";
-import MainsunhoLee from "./pages/sunhoLee/Main/Main";
+import LoginSunho from "./pages/sunhoLee/Login/Login";
+import MainSunho from "./pages/sunhoLee/Main/Main";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/login-minji" component={LoginminjiJeong} />
-          <Route exact path="/main-minji" component={MainminjiJeong} />
-          <Route exact path="/login-seyeon" component={LoginseyeonPark} />
-          <Route exact path="/main-seyeon" component={MainseyeonPark} />
-          <Route exact path="/login-sunho" component={LoginsunhoLee} />
-          <Route exact path="/main-sunho" component={MainsunhoLee} />
-          <Route exact path="/login-suminKim" component={LoginsuminKim} />
-          <Route exact path="/main-suminKim" component={MainsuminKim} />
+          <Route exact path="/login-minji" component={LoginMinji} />
+          <Route exact path="/main-minji" component={MainMinji} />
+          <Route exact path="/login-seyeon" component={LoginSeyeon} />
+          <Route exact path="/main-seyeon" component={MainSeyeon} />
+          <Route exact path="/login-sunho" component={LoginSunho} />
+          <Route exact path="/main-sunho" component={MainSunho} />
+          <Route exact path="/login-suminKim" component={LoginSumin} />
+          <Route exact path="/main-suminKim" component={MainSumin} />
         </Switch>
       </Router>
     );
